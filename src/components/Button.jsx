2fdf94f74d24a0e7864f7ee2b00e788605513e03
Unknown <victor.graffard@gmail.com>
2019-03-24 23:21:59 +0100

    Oopsie, Button reworked (10% of brain capacity left)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import classNames from 'classnames'
 
-const Button = props => {
-  const btn = props.disabled ? (
-    <button disabled className={classNames('button', { [`${props.customClass}`]: props.customClass })} onClick={props.buttonClicked}>
-      {props.name}
-    </button>
-  ) : (
-    <button className={classNames('button', { [`${props.customClass}`]: props.customClass })} onClick={props.buttonClicked}>
-      {props.name}
-    </button>
-  )
-
-  return btn
-}
+const Button = props => (
+  <button
+    disabled={props.disabled}
+    className={classNames('button', { [`${props.customClass}`]: props.customClass })}
+    onClick={props.buttonClicked}
+  >
+    {props.name}
+  </button>
+)
 
 export default Button
